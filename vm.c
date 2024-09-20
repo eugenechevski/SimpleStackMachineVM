36bd835654391ddf5750766e417098b053c95007
MarkMango21 <mark2asf@gmail.com>
2024-09-19 17:32:21 -0700

    first things
    I started of by creating the stack, dynamically allocating it, in case we use many functions and need to point it  into other functions

--- vm.c
+++ vm.c
@@ -4,13 +4,25 @@
 #include "machine_types.h"
 #include "instruction.h"
 #include "bof.h"
-
+#include <string.h>
+#define STACK_SIZE 4096
+#define DATA_SIZE 1024
 // TODO: Implement VM functions here
 
-int main(int argc, char *argv[]) {
-    // TODO: Implement main function
-    // Parse arguments
-    // Load BOF file
-    // Execute or print program based on arguments
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <file.asm>\n", argv[0]);
+        return 1;
+    }
+    int *stack = malloc(STACK_SIZE * sizeof(int));
+    if (!stack)
+    {
+        printf("Error: Could not allocate memory for stack\n");
+        return 1;
+    }
+    int sp = STACK_SIZE;
+    free(stack);
     return 0;
 }
